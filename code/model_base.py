4ff161acedf1062b7b3354630ffa667a35b17082
bcx_akira <bcx_akira@163.com>
2019-08-13 19:23:11 +0800

    融合模型:均值 基本写完

--- code/model_base.py
+++ code/model_base.py
@@ -8,10 +8,12 @@ import pickle
 import time
 import json
 from multiprocessing import Pool
+from typing import List, Dict, Sequence, Optional, Any
 
 import pandas as pd
+import numpy as np
 import sklearn
-from sklearn import linear_model, model_selection
+from sklearn import linear_model, tree, ensemble, svm, model_selection
 from sklearn.metrics import mean_squared_error
 
 from utility import timer, now
@@ -19,7 +21,7 @@ from utility import timer, now
 LOG = logging.getLogger('my')
 
 
-def return_feature(method: int) -> list:
+def return_feature(method: int) -> List[str]:
     """
     读取原始文件，根据method，返回特征集合
 
@@ -57,7 +59,7 @@ class FeatureManager:
 
     features = {}  # 特征集合
 
-    def add_feature(self, name: str, feature: list):
+    def add_feature(self, name: str, feature: List[str]):
         """
         给features添加一个元素
 
@@ -95,12 +97,24 @@ class ModelManager:
         elif name == 'RidgeRegression':
             model = linear_model.Ridge(**kwargs)
 
+        elif name == 'DecisionTreeRegressor':
+            model = tree.DecisionTreeRegressor(**kwargs)
+
+        elif name =='GradientBoostingRegressor':
+            model = ensemble.GradientBoostingRegressor(**kwargs)
+
+        elif name == 'RandomForestRegressor':
+            model = ensemble.RandomForestRegressor(**kwargs)
+
+        elif name == 'SVR':
+            model = svm.SVR(**kwargs)
+
         else:
             raise Exception('name输入错误')
 
         self.models[name] = model
 
-    def update_param_space(self, name: str, param: dict):
+    def update_param_space(self, name: str, param: Dict[str, list]):
         """
         给param_spaces添加或修改一个元素
 
@@ -122,6 +136,8 @@ class Manager(FeatureManager, ModelManager):
 
     source_train = pd.DataFrame()  # 原始训练集
     test_set = pd.DataFrame()  # 测试集
+    groups = {}  # 交叉验证数据分组索引
+    cv_predict = pd.DataFrame()  # 模型交叉验证预测值
     adjust_result = pd.DataFrame()  # 调参结果
     validate_result = pd.DataFrame()  # 验证结果
     test_result = pd.DataFrame()  # 测试结果
@@ -154,7 +170,53 @@ class Manager(FeatureManager, ModelManager):
         test_path = r'D:\study\env\steam\data\source_data\zhengqi_test.txt'
         self.test_set = pd.read_table(test_path, sep='\t')
 
-    def adjust(self, feature_list: list, model_list: list):
+    def create_groups(self):
+        """
+        返回分组对应的训练集，验证集
+
+        :return: 每组对应的训练集验证集索引
+        """
+
+        index = self.source_train.index
+        source_train_group = pd.Series([i % 5 for i in index], index=index)
+        groups = {}
+        fold = [0, 1, 2, 3, 4]
+
+        for group in fold:
+            fold_temp = fold.copy()
+            fold_temp.remove(group)
+            train_index = list(
+                source_train_group[source_train_group.isin(fold_temp)].index)
+            validation_index = list(
+                source_train_group[source_train_group == group].index)
+            groups[group] = {
+                'train': train_index,
+                'validation': validation_index
+            }
+
+        self.groups = groups
+
+    @staticmethod
+    def update_class_result(class_result, result, feature_list, model_list):
+        if class_result.empty:
+            class_result = class_result.append(result)
+
+        else:
+            if_exist = (class_result['model_name'].isin(model_list)) \
+                       & (class_result['feature_name'].isin(feature_list))
+
+            if if_exist.sum():
+                exist_index = if_exist[if_exist].index
+                class_result.drop(index=exist_index, inplace=True)
+
+            class_result = class_result.append(result)
+
+        class_result.sort_values(['feature_name', 'model_name'], inplace=True)
+        class_result.reset_index(drop=True, inplace=True)
+        return class_result
+
+    @timer
+    def adjust(self, feature_list: List[str], model_list: List[str]):
         """
         模型调参
 
@@ -179,7 +241,7 @@ class Manager(FeatureManager, ModelManager):
                 param_space = self.param_spaces[model_name]
 
                 gscv = model_selection.GridSearchCV(
-                    model, param_space, cv=5, n_jobs=None, refit=False)
+                    model, param_space, cv=5, refit=False)
                 start_time = time.time()
                 gscv.fit(data, target)
                 end_time = time.time()
@@ -204,52 +266,11 @@ class Manager(FeatureManager, ModelManager):
         LOG.info('调参结束')
 
         result['update_time'] = now()
-        adjust_result = self.adjust_result
-
-        if adjust_result.empty:
-            adjust_result = adjust_result.append(result)
-
-        else:
-            if_exist = (adjust_result['model_name'].isin(model_list)) \
-                       & (adjust_result['feature_name'].isin(feature_list))
-
-            if if_exist.sum():
-                exist_index = if_exist[if_exist].index
-                adjust_result.drop(index=exist_index, inplace=True)
-
-            adjust_result = adjust_result.append(result)
-
-        adjust_result.sort_values(['feature_name', 'model_name'], inplace=True)
-        adjust_result.reset_index(drop=True, inplace=True)
+        adjust_result = self.update_class_result(
+            self.adjust_result, result, feature_list, model_list)
         self.adjust_result = adjust_result
 
-    def return_groups(self) -> dict:
-        """
-        返回分组对应的训练集，验证集
-
-        :return: 每组对应的训练集验证集
-        """
-
-        index = self.source_train.index
-        source_train_group = pd.Series([i % 5 for i in index], index=index)
-        groups = {}
-        fold = [0, 1, 2, 3, 4]
-
-        for group in fold:
-            fold_temp = fold.copy()
-            fold_temp.remove(group)
-            train_index = (
-                source_train_group[source_train_group.isin(fold_temp)].index)
-            validation_index = (
-                source_train_group[source_train_group == group].index)
-            groups[group] = {
-                'train': train_index,
-                'validation': validation_index
-            }
-
-        return groups
-
-    def return_model(self, model_name: str, feature_name: str):
+    def return_model(self, model_name: str, feature_name: str) -> sklearn.base:
         """
         返回模型
 
@@ -269,15 +290,19 @@ class Manager(FeatureManager, ModelManager):
 
     @staticmethod
     def k_fold_validation(
-            groups: dict,
+            feature_name: str,
+            model_name: str,
+            groups: Dict[int, Dict[str, Sequence]],
             group: int,
             data: pd.DataFrame,
             target: pd.Series,
             model: sklearn.base
-    ) -> dict:
+    ) -> Dict[str, Any]:
         """
         返回每折数据的验证结果
 
+        :param feature_name: 特征集名
+        :param model_name: 模型名
         :param groups: 每组对应的训练集验证集
         :param group: 组号
         :param data: 特征数据
@@ -285,7 +310,6 @@ class Manager(FeatureManager, ModelManager):
         :param model: 训练的模型
         :return: 该组对应的验证结果
         """
-        print('pid:', os.getpid())
 
         train_index = groups[group]['train']
         validation_index = groups[group]['validation']
@@ -299,8 +323,8 @@ class Manager(FeatureManager, ModelManager):
         model.fit(train, train_target)
         end_time = time.time()
 
-        train_predict = model.predict(train)
-        validation_predict = model.predict(validation)
+        train_predict = list(model.predict(train))
+        validation_predict = list(model.predict(validation))
 
         train_error = mean_squared_error(train_target,
                                          train_predict)
@@ -310,13 +334,51 @@ class Manager(FeatureManager, ModelManager):
         run_time = end_time - start_time
 
         model_result = {
+            'feature_name': feature_name,
+            'model_name': model_name,
+            'group': group,
+            'train_predict': train_predict,
+            'validation_predict': validation_predict,
             'train_error': train_error,
             'validation_error': validation_error,
-            'run_time': run_time
+            'run_time': run_time,
         }
         return model_result
 
-    def validate(self, feature_list: list, model_list: list):
+    def append_validate_result(self,
+                               feature_name: str,
+                               name: str,
+                               score_train: float,
+                               score_validation: float,
+                               mean_train_time: Optional[float],
+                               param: str):
+        """
+        往self.validate_result里添加一条记录
+
+        :param feature_name: 特征名
+        :param name: 模型名或融合名
+        :param score_train: 训练集平均得分
+        :param score_validation: 验证集平均得分
+        :param mean_train_time: 平均训练时长
+        :param param: 模型或融合的参数
+        :return:
+        """
+
+        validate_result = {
+            'feature_name': feature_name,
+            'name': name,
+            'score_train': score_train,
+            'score_validation': score_validation,
+            'mean_train_time': mean_train_time,
+            'param': param,
+            'update_time': now()
+        }
+
+        self.validate_result = self.validate_result.append(
+            validate_result, ignore_index=True)[validate_result.keys()]
+
+    @timer
+    def model_validate(self, feature_list: List[str], model_list: List[str]):
         """
         模型验证
 
@@ -325,7 +387,8 @@ class Manager(FeatureManager, ModelManager):
         :return: None
         """
 
-        groups = self.return_groups()
+        groups = self.groups
+        cv_predict_result = pd.DataFrame()
 
         for feature_name in feature_list:
 
@@ -344,7 +407,8 @@ class Manager(FeatureManager, ModelManager):
                 for group in groups.keys():
                     group_result = pool.apply_async(
                         self.k_fold_validation,
-                        args=(groups, group, data, target, model)
+                        args=(feature_name, model_name, groups, group,
+                              data, target, model)
                     )
                     result.append(group_result)
                 pool.close()
@@ -352,27 +416,99 @@ class Manager(FeatureManager, ModelManager):
                 result = [group_result.get() for group_result in result]
                 result = pd.DataFrame(result)
 
-                score_train = result['train_error'].mean()
-                score_validation = result['validation_error'].mean()
-                mean_train_time = result['run_time'].mean()
-                param = json.dumps(model.get_params())
+                self.append_validate_result(
+                    feature_name=feature_name,
+                    name=model_name,
+                    score_train=result['train_error'].mean(),
+                    score_validation=result['validation_error'].mean(),
+                    mean_train_time=result['run_time'].mean(),
+                    param=json.dumps(model.get_params())
+                )
 
-                result = {
-                    'feature_name': feature_name,
-                    'name': model_name,
-                    'score_train': score_train,
-                    'score_validation': score_validation,
-                    'mean_train_time': mean_train_time,
-                    'param': param,
-                    'update_time': now()
-                }
-
-                self.validate_result = self.validate_result.append(
-                    result, ignore_index=True)[result.keys()]
+                col = ['feature_name', 'model_name', 'group', 'train_predict',
+                       'validation_predict']
+                cv_predict_result = cv_predict_result.append(result[col])
 
         LOG.info('验证结束')
 
-    def model_test(self, feature_list: list, model_list: list):
+        cv_predict = self.update_class_result(
+            self.cv_predict, cv_predict_result, feature_list, model_list)
+        self.cv_predict = cv_predict
+
+    def merge_validate(self,
+                       feature_list: List[str],
+                       merge_list: List[str],
+                       model_list: List[str]):
+        """
+        融合验证
+
+        :param feature_list: 特征集合
+        :param merge_list: 融合集合
+        :param model_list: 融合需要的模型集合
+        :return:
+        """
+
+        groups = self.groups
+        cv_predict = self.cv_predict
+
+        for feature_name in feature_list:
+
+            target = self.source_train['target']
+            choose = ((cv_predict['feature_name'] == feature_name)
+                      & (cv_predict['model_name'].isin(model_list)))
+            predict = cv_predict.loc[choose]
+
+            for merge_name in merge_list:
+
+                LOG.info(f'开始验证 {feature_name} {merge_name}')
+
+                if merge_name == 'Mean':
+
+                    result = pd.DataFrame()
+
+                    for group in groups.keys():
+
+                        group_predict = predict.loc[predict['group'] == group]
+
+                        for item in ['train', 'validation']:
+                            item_predict = np.array(list(
+                                group_predict[item + '_predict'].values))
+                            mean_predict = item_predict.mean(axis=0)
+                            true_value = target[groups[group][item]]
+                            score = mean_squared_error(true_value, mean_predict)
+                            result.loc[group, item] = score
+
+                    self.append_validate_result(
+                        feature_name=feature_name,
+                        name=merge_name,
+                        score_train=result['train'].mean(),
+                        score_validation=result['validation'].mean(),
+                        mean_train_time=None,
+                        param=json.dumps(model_list)
+                    )
+
+        LOG.info('验证结束')
+
+    def append_test_result(self, feature_name, name, param, time_now):
+        result = {
+            'feature_name': feature_name,
+            'name': name,
+            'score': None,
+            'param': param,
+            'update_time': time_now
+        }
+
+        self.test_result = self.test_result.append(
+            result, ignore_index=True)[result.keys()]
+
+    def test_out(self, predict, time_now, name, feature_name):
+        test_predict = pd.Series(predict)
+        file_name = '+'.join([time_now, name, feature_name]) + '.txt'
+        path = os.path.join(self.test_predict_path, file_name)
+        test_predict.to_csv(path, index=False, header=False)
+
+    @timer
+    def model_test(self, feature_list: List[str], model_list: List[str]):
         """
         模型测试
 
@@ -392,30 +528,64 @@ class Manager(FeatureManager, ModelManager):
 
             for model_name in model_list:
                 LOG.info(f'开始测试集预测 {feature_name} {model_name}')
-                model = self.return_model(model_name, feature_name)
 
+                model = self.return_model(model_name, feature_name)
                 model.fit(train_set, target)
 
                 time_now = now()
                 param = json.dumps(model.get_params())
 
-                result = {
-                    'feature_name': feature_name,
-                    'name': model_name,
-                    'score': None,
-                    'param': param,
-                    'update_time': time_now
-                }
+                self.append_test_result(
+                    feature_name, model_name, param, time_now)
 
-                self.test_result = self.test_result.append(
-                    result, ignore_index=True)[result.keys()]
-
-                test_predict = pd.Series(model.predict(test_set))
-                name = '+'.join([time_now, model_name, feature_name]) + '.txt'
-                path = os.path.join(self.test_predict_path, name)
-                test_predict.to_csv(path, index=False, header=False)
+                predict = model.predict(test_set)
+                self.test_out(predict, time_now, model_name, feature_name)
 
         LOG.info(f'测试集预测结束')
+
+    @timer
+    def merge_test(self,
+                   feature_list: List[str],
+                   merge_list: List[str],
+                   model_list: List[str]):
+        """
+        融合测试
+
+        :param feature_list: 特征集合
+        :param merge_list: 融合集合
+        :param model_list: 融合需要的模型集合
+        :return:
+        """
+
+        for feature_name in feature_list:
+
+            feature = self.features[feature_name]
+
+            train_set = self.source_train[feature]
+            target = self.source_train['target']
+
+            test_set = self.test_set[feature]
+
+            for merge_name in merge_list:
+
+                LOG.info(f'开始测试集预测 {feature_name} {merge_name}')
+
+                if merge_name == 'Mean':
+
+                    result = []
+                    for model_name in model_list:
+                        model = self.return_model(model_name, feature_name)
+                        model.fit(train_set, target)
+                        model_predict = model.predict(test_set)
+                        result.append(model_predict)
+
+                    time_now = now()
+                    param = json.dumps(model_list)
+                    self.append_test_result(
+                        feature_name, merge_name, param, time_now)
+
+                    predict = np.array(result).mean(axis=0)
+                    self.test_out(predict, time_now, merge_name, feature_name)
 
     def write_class_variable(self):
         """
@@ -428,7 +598,9 @@ class Manager(FeatureManager, ModelManager):
             'features': self.features,
             'models': self.models,
             'param_spaces': self.param_spaces,
-            'adjust_result': self.adjust_result
+            'adjust_result': self.adjust_result,
+            'cv_predict': self.cv_predict,
+            'groups': self.groups
         }
 
         with open(self.pickle_path, 'wb') as f:
@@ -455,6 +627,8 @@ class Manager(FeatureManager, ModelManager):
         self.models = object_pickle['models']
         self.param_spaces = object_pickle['param_spaces']
         self.adjust_result = object_pickle['adjust_result']
+        self.cv_predict = object_pickle['cv_predict']
+        self.groups = object_pickle['groups']
 
         self.validate_result = pd.read_excel(self.validate_path)
         self.test_result = pd.read_excel(self.test_path)
--- code/model_dev.py
+++ code/model_dev.py
@@ -4,46 +4,128 @@
 
 import logging.config
 
+import numpy as np
+
 from model_base import return_feature, Manager
 
-if __name__ == '__main__':
+logging.config.fileConfig(r'D:\study\env\steam\log.ini')
 
-    logging.config.fileConfig(r'D:\study\env\steam\log.ini')
+LOG = logging.getLogger('my')
 
-    LOG = logging.getLogger('my')
 
-    manager = Manager()
-
+def add_feature():
     for method in [1, 2, 3]:
         manager.add_feature(str(method), return_feature(method))
 
+
+def update_model():
     manager.update_model('LinearRegression')
     manager.update_model('LassoRegression')
     manager.update_model('RidgeRegression')
+    manager.update_model('DecisionTreeRegressor')
+    manager.update_model('GradientBoostingRegressor',
+                         min_samples_leaf=5, n_iter_no_change=5)
+    manager.update_model('RandomForestRegressor', min_samples_leaf=5)
+    manager.update_model('SVR', gamma='scale')
 
-    manager.update_param_space('LinearRegression', {'normalize': [True, False]})
-    manager.update_param_space('LassoRegression', {'alpha': [0.5, 1, 2]})
-    manager.update_param_space('RidgeRegression', {'alpha': [0.5, 1, 2]})
 
-    manager.read_source_train()
-    manager.read_test_set()
+def update_param_space():
+    manager.update_param_space('LinearRegression', {
+        'normalize': [True, False]
+    })
 
-    manager.adjust(['1', '2', '3'],
-                   ['LinearRegression', 'LassoRegression', 'RidgeRegression'])
+    manager.update_param_space('LassoRegression', {
+        'alpha': [round(i, 1) for i in np.linspace(0.1, 50, 100)]
+    })
 
-    manager.validate(['1'],
-                     ['LinearRegression', 'LassoRegression'])
+    manager.update_param_space('RidgeRegression', {
+        'alpha': [round(i, 1) for i in np.linspace(0.1, 50, 100)]
+    })
 
-    manager.model_test(['3'],
-                       ['RidgeRegression'])
+    manager.update_param_space('DecisionTreeRegressor', {
+        'min_samples_leaf': [int(i) for i in np.linspace(1, 100, 100)],
+    })
+
+    manager.update_param_space('GradientBoostingRegressor', {
+        'n_estimators': [500],
+        'learning_rate': [0.01, 0.02, 0.03, 0.05, 0.1],
+        'max_depth': [3, 4, 5],
+        'subsample': [0.7],
+        'max_features': [0.7],
+    })
+
+    manager.update_param_space('RandomForestRegressor', {
+        'n_estimators': [300, 400, 500],
+        'max_depth': [3, 4, 5, 6],
+        'max_features': [0.8, 0.7, 0.6, 0.5],
+    })
+
+    manager.update_param_space('SVR', {
+        'kernel': ['linear', 'poly'],
+        'C': [round(i, 1) for i in np.linspace(0.1, 2, 20)]
+    })
+
+
+if __name__ == '__main__':
+
+    manager = Manager()
+
+    manager.read_class_variable()
+
+    # add_feature()
+    # update_model()
+    # update_param_space()
+
+    # manager.read_source_train()
+    # manager.read_test_set()
+    # manager.create_groups()
+
+    manager.adjust(
+        ['2', '3'],
+        ['SVR']
+    )
+
+    # ['LinearRegression', 'LassoRegression', 'RidgeRegression',
+    #  'DecisionTreeRegressor', 'GradientBoostingRegressor',
+    #  'RandomForestRegressor', 'SVR']
+
+    manager.model_validate(
+        ['2', '3'],
+        ['LinearRegression', 'LassoRegression', 'RidgeRegression',
+         'DecisionTreeRegressor', 'GradientBoostingRegressor',
+         'RandomForestRegressor', 'SVR']
+    )
+
+    manager.merge_validate(
+        ['2', '3'],
+        ['Mean'],
+        ['LinearRegression', 'LassoRegression', 'RidgeRegression',
+         'DecisionTreeRegressor', 'GradientBoostingRegressor',
+         'RandomForestRegressor', 'SVR']
+    )
+
+    manager.model_test(
+        ['2'],
+        ['GradientBoostingRegressor']
+    )
+
+    manager.merge_test(
+        ['2'],
+        ['Mean'],
+        ['LinearRegression', 'LassoRegression', 'RidgeRegression',
+         'DecisionTreeRegressor', 'GradientBoostingRegressor',
+         'RandomForestRegressor', 'SVR']
+    )
 
     manager.write_class_variable()
-    manager.read_class_variable()
 
     print('manager.features \n', manager.features, '\n')
     print('manager.models \n', manager.models, '\n')
     print('manager.param_spaces \n', manager.param_spaces, '\n')
     print('manager.source_train \n', manager.source_train, '\n')
+    print('manager.test_set \n', manager.test_set, '\n')
+    print('manager.groups \n', manager.groups, '\n')
+    print('manager.cv_predict \n', manager.cv_predict, '\n')
     print('manager.adjust_result \n', manager.adjust_result, '\n')
     print('manager.validate_result \n', manager.validate_result, '\n')
     print('manager.test_result \n', manager.test_result, '\n')
